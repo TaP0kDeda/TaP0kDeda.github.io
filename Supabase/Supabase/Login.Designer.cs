757d6e009459e4729fc3f5b08fa65abb3a2b356a
TaP0kDeda <gerascooter2000@gmail.com>
2026-03-16 14:19:52 +0300

    Added part of supabase code DOESN'T WORK CORRECTLY, NEED TO FIXED

--- Supabase/Supabase/Login.Designer.cs
+++ Supabase/Supabase/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Supabase
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -35,16 +35,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Location = new System.Drawing.Point(61, 250);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(348, 41);
             this.button1.TabIndex = 0;
             this.button1.Text = "Войти";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -54,17 +58,18 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Зарегистрироваться";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(61, 83);
+            this.textBox1.Location = new System.Drawing.Point(61, 47);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(348, 20);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(61, 153);
+            this.textBox2.Location = new System.Drawing.Point(61, 117);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(348, 20);
             this.textBox2.TabIndex = 3;
@@ -72,7 +77,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 60);
+            this.label1.Location = new System.Drawing.Point(58, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 4;
@@ -81,7 +86,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(58, 131);
+            this.label2.Location = new System.Drawing.Point(58, 95);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 5;
@@ -90,17 +95,26 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(218, 204);
+            this.label3.Location = new System.Drawing.Point(217, 153);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "label3";
             // 
-            // Form1
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(202, 178);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(61, 57);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(482, 425);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -108,9 +122,10 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Login";
+            this.Text = "Вход";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,6 +140,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
---
+++ Supabase/Supabase/Register.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Supabase.Login;
+
+namespace Supabase
+{
+    public partial class Register : Form
+    {
+
+        private string AvatarPath = "";
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.jpg;*.png;*.jpeg";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    AvatarPath = ofd.FileName;
+                    pictureBox1.Image = System.Drawing.Image.FromFile(AvatarPath);
+                }
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text.Trim();
+            string password = textBox2.Text.Trim();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Логин или пароль не заполнены!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                string avatar_url = "";
+                if (!string.IsNullOrEmpty(AvatarPath))
+                {
+                    byte[] fileBytes = File.ReadAllBytes(AvatarPath);
+                    string fileName = $"{username}_avatar{Path.GetExtension(AvatarPath)}";
+
+                    await Login.Client.Storage.From("avatars").Upload(fileBytes, fileName);
+                    avatar_url = Login.Client.Storage.From("avatars").GetPublicUrl(fileName);
+                }
+
+                var newUser = new NewUser { username = username, password = password, avatar_url = avatar_url };
+                await Login.Client.From<NewUser>().Insert(newUser);
+
+                MessageBox.Show("Регистрация успешна, пройдите на окно входа", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка при регистрации: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
