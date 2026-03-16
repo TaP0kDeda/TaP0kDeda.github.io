f67d4061899e57c0a008a69d961b63fe7d8ba119
TaP0kDeda <gerascooter2000@gmail.com>
2026-03-16 12:46:59 +0300

    Added form for supabase proj

---
+++ Supabase/Supabase/Form1.cs
@@ -0,0 +1,30 @@
+﻿using Supabase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace Supabase
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
